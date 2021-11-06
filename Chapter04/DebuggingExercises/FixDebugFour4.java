c2032b19531205797ed4a1a945a50cd38867ba89
Animecows <93840645+Animecows@users.noreply.github.com>
2021-11-06 16:15:10 -0400

    I might be wrong but I tried
    It should work but not the right way

--- Chapter04/DebuggingExercises/FixDebugFour4.java
+++ Chapter04/DebuggingExercises/FixDebugFour4.java
@@ -1,9 +1,9 @@
 // This class discounts prices by 10%
-public class FixDebugFour4
+public class DebugFour4
 {
    public static void main(String args[])
    {
-      final double DISCOUNT_RATE = 0.90;
+      final double DISCOUNT_RATE = .10;
       int price = 100;
       double price2 = 100.00;
       tenPercentOff(price, DISCOUNT_RATE);
@@ -11,14 +11,14 @@ public class FixDebugFour4
    }
    public static void tenPercentOff(int p, final double DISCOUNT_RATE)
    {
-      double newPrice = p * DISCOUNT_RATE;
-      System.out.println("Ten percent off " + p);
-      System.out.println("  New price is " + newPrice);
+      double newPrice = p / DISCOUNT_RATE;
+      System.out.println(" Ten percent off "  + p);
+      System.out.println("  New price is " + (p - 10));
    }
    public static void tenPercentOff(double p, final double DISCOUNT_RATE)
    {
-      double newPrice = p * DISCOUNT_RATE;
-      System.out.println("Ten percent off " + p);
-      System.out.println("  New price is " + newPrice);
+      double newPrice = p / DISCOUNT_RATE;
+      System.out.println(" Ten percent off " + p);
+      System.out.println("  New price is " + (p - 10));
    }
 }
